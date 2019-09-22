00e02d58230d1c6d2723696f832ccdc8393cfb25
Rustam Drake <rustamdrake@gmail.com>
2019-09-22 23:15:08 +0100

    second commit

---
+++ src/main/java/com/zaichick/user/entrypoint/CreateUser.java
@@ -0,0 +1,12 @@
+package com.zaichick.user.entrypoint;
+
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+
+public class CreateUser {
+
+    public AwsProxyResponse handleRequest(AwsProxyRequest awsProxyRequest) {
+
+        return null;
+    }
+}
