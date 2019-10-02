37834369dca58b464c813622e0b2ec184d85ab2e
ali drake <alidrake01@gmail.com>
2019-10-02 13:28:02 -0700

    added get method

---
+++ src/main/java/com/zaichick/user/entrypoint/FindUser.java
@@ -0,0 +1,36 @@
+package com.zaichick.user.entrypoint;
+
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zaichick.user.dao.UserDao;
+import com.zaichick.user.domain.User;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class FindUser {
+
+    private final UserDao userDao;
+    private final ObjectMapper objectMapper;
+
+    public FindUser() {
+        userDao = new UserDao();
+        objectMapper = new ObjectMapper();
+    }
+
+    public AwsProxyResponse handleRequest(AwsProxyRequest awsProxyRequest) throws IOException {
+        Map<String, String> queryParamMap = awsProxyRequest.getQueryStringParameters();
+        List<User> userByEmailAddress = userDao.findUserByEmailAddress(queryParamMap.get("email"));
+
+        AwsProxyResponse awsProxyResponse = new AwsProxyResponse();
+        String responseBody = objectMapper.writeValueAsString(userByEmailAddress);
+        awsProxyResponse.setBody(responseBody);
+        awsProxyResponse.setStatusCode(200);
+
+        return awsProxyResponse;
+    }
+
+
+}
